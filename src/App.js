5b7b302d1d22f5bc465a6fe9322651958ff7c24f
David <davidyu37@gmail.com>
2018-11-26 11:54:41 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WechatJSSDK from 'wechat-jssdk/dist/client';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      //below are mandatory options to finish the wechat signature verification
+      //the 4 options below should be received like api '/get-signature' above
+      'appId': 'app_id',
+      'nonceStr': 'your_nonceStr',
+      'signature': 'url_signature',
+      'timestamp': 'your_timestamp',
+      //below are optional
+      //invoked if wechat signature sign succeeds,
+      //'this' will be the jssdk instance if it's a normal function, 
+      // in v3.0.10+, jssdk instance will be passed to the callback, (wxObj) => {}
+      // in the up coming v4, "success"/"error" init callback will be replace by #initialize() which will return Promise, see below
+      'success': jssdkInstance => { },
+      //invoked if sign failed, in v3.0.10+, jssdk instance will be pass to the func, (err, wxObj) => {}
+      'error': (err, jssdkInstance) => { },
+      //enable debug mode, same as debug
+      'debug': true,
+      'jsApiList': [], //optional, pass all the jsapi you want, the default will be ['onMenuShareTimeline', 'onMenuShareAppMessage']
+      'customUrl': '' //set custom weixin js script url, usually you don't need to add this js manually
+    }
+    const wechatObj = new WechatJSSDK(config);
+  }
+
   render() {
     return (
       <div className="App">
