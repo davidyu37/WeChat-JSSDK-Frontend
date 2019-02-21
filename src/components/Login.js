80aa06c8212a2177b322fe3960074947c57ee408
David <davidyu37@gmail.com>
2019-02-21 18:21:11 +0800

    login start

---
+++ src/components/Login.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import ReactLogo from '../react.png';
+
+class Share extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+
+
+  render() {
+    return (
+        <div className="btn-container">
+        </div>
+    );
+  }
+}
+
+export default Share;
